2f273570ebfdee4661652a519db16484db3d084e
Jon Shad <shad_stick@hotmail.co.uk>
2017-04-18 21:45:27 +0100

    added Basket model method to total basket amount

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -3,6 +3,7 @@ class BasketsController < ApplicationController
 
   def index
     @basket_contents = Basket.all
+    @basket_total = Basket.basket_total
   end
 
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,2 +1,11 @@
 class Basket < ApplicationRecord
+
+  def self.basket_total
+    total = 0.00
+    Basket.all.each do |item|
+      total += item.price
+    end
+    '%.2f' % total
+  end
+
 end
